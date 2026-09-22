1c3c215544537cdffd6976f1bab72a82a8465e62
Louis Padillo <johnchrislouis@gmail.com>
2026-09-22 15:54:35 -0600

    added farbod's github in student-info.js (not luiz's repo)

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -7,6 +7,10 @@ export default function StudentInfo() {
             <Link href="https://github.com/louispadillo">My Github: louispadillo</Link>
             <h1>Name: Luiz Felipe Correra</h1>
             <Link href="https://github.com/lipe22cwb">My Github: lipe22cwb</Link>
+            <h1>Name: Farbod Rahimzadeh</h1>
+            <Link href="https://github.com/Farbod-sait">
+                My Github: farbod-sait
+            </Link>
         </main>
     );
 }
