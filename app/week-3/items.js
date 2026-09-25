da7a95532b9a5b7d25454f36146ee65aa20ab5ab
Farbod-sait <farbod.rahimzadeh@edu.sait.ca>
2026-09-25 14:49:25 -0600

    Update items.js

--- app/week-3/items.js
+++ app/week-3/items.js
@@ -1,71 +1,81 @@
-const item1 = {
-  name: "milk, 4 L 🥛",
-  quantity: 1,
-  category: "dairy",
-};
+import Item from "./item";
 
-const item2 = {
-  name: "bread 🍞",
-  quantity: 2,
-  category: "bakery",
-};
+export default function ItemList() {
 
-const item3 = {
-  name: "eggs, dozen 🥚",
-  quantity: 2,
-  category: "dairy",
-};
+    const items = [
+        {
+            name: "milk, 4 L 🥛",
+            quantity: 1,
+            category: "dairy",
+        },
+        {
+            name: "bread 🍞",
+            quantity: 2,
+            category: "bakery",
+        },
+        {
+            name: "eggs, dozen 🥚",
+            quantity: 2,
+            category: "dairy",
+        },
+        {
+            name: "bananas 🍌",
+            quantity: 6,
+            category: "produce",
+        },
+        {
+            name: "broccoli 🥦",
+            quantity: 3,
+            category: "produce",
+        },
+        {
+            name: "chicken breasts, 1 kg 🍗",
+            quantity: 1,
+            category: "meat",
+        },
+        {
+            name: "pasta sauce 🍝",
+            quantity: 3,
+            category: "canned goods",
+        },
+        {
+            name: "spaghetti, 454 g 🍝",
+            quantity: 2,
+            category: "dry goods",
+        },
+        {
+            name: "toilet paper, 12 pack 🧻",
+            quantity: 1,
+            category: "household",
+        },
+        {
+            name: "paper towels, 6 pack",
+            quantity: 1,
+            category: "household",
+        },
+        {
+            name: "dish soap 🍽️",
+            quantity: 1,
+            category: "household",
+        },
+        {
+            name: "hand soap 🧼",
+            quantity: 4,
+            category: "household",
+        }
+    ];
 
-const item4 = {
-  name: "bananas 🍌",
-  quantity: 6,
-  category: "produce",
-};
 
-const item5 = {
-  name: "broccoli 🥦",
-  quantity: 3,
-  category: "produce",
-};
-
-const item6 = {
-  name: "chicken breasts, 1 kg 🍗",
-  quantity: 1,
-  category: "meat",
-};
-
-const item7 = {
-  name: "pasta sauce 🍝",
-  quantity: 3,
-  category: "canned goods",
-};
-
-const item8 = {
-  name: "spaghetti, 454 g 🍝",
-  quantity: 2,
-  category: "dry goods",
-};
-
-const item9 = {
-  name: "toilet paper, 12 pack 🧻",
-  quantity: 1,
-  category: "household",
-};
-
-const item10 = {
-  name: "paper towels, 6 pack",
-  quantity: 1,
-  category: "household",
-};
-
-const item11 = {
-  name: "dish soap 🍽️",
-  quantity: 1,
-  category: "household",
-};
-
-const item12 = {
-  name: "hand soap 🧼",
-  quantity: 4,
-  category: "household",
-};
+    return (
+        <ul>
+            {items.map((item, index) => (
+                <Item
+                    key={index}
+                    name={item.name}
+                    quantity={item.quantity}
+                    category={item.category}
+                />
+            ))}
+        </ul>
+    );
+}
