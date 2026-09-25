3f3db5f6f285a2d51404b0041c1c7526aca6168e
Farbod-sait <farbod.rahimzadeh@edu.sait.ca>
2026-09-25 14:50:04 -0600

    Refactor Page component to use ItemList
    Removed hardcoded dog information and replaced with ItemList component.

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -1,23 +1,15 @@
-import GoBack from "../go-back";
+import ItemList from "./item-list";
 
 export default function Page() {
-    const dog1 = {
-        name: "Yin Fang",
-        age: 2,
-        breed: "Shitzu",
-        color: "Yellow"
-    }
     return (
-        <main>
-            <h1 className="text-4xl text-red-500">Week 3 - Components and Props</h1>
-            <h2 className="text-3xl text-blue-600">Dogs Information</h2>
-            <section className="bg-slate-300 w-100">
-                <h2 className="font-bold">{dog1.name}</h2>
-                <p className="ml-4">Age: {dog1.age}</p>
-                <p className="ml-4">Breed: {dog1.breed}</p>
-                <p className="ml-4">Color: {dog1.color}</p>
-            </section>
-            <GoBack />
+        <main className="min-h-screen bg-slate-100 p-8">
+
+            <h1 className="text-4xl font-bold text-blue-600 mb-6">
+                Shopping List
+            </h1>
+
+            <ItemList />
+
         </main>
     );
 }
